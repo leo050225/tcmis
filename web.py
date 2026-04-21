dd98e3eb4bd9998f30fb296ba9a9f53fe74b09da
leo050225 <leo050225@gmail.om>
2026-04-21 12:23:02 +0800

    v6

--- web.py
+++ web.py
@@ -37,8 +37,10 @@ def index():
     homepage += "<a href=/math>數學運算</a><br>"
     homepage += "<a href=/about>聿觀簡介網頁</a><br>"
     homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
-    homepage += "<br><a href=/search>關鍵字搜尋資料</a><br>"
-    homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
+    homepage += "<br><a href=/search>搜尋資料</a><br>"
+    homepage += "<br><a href=/movie>讀取開眼電影即將上映影片</a><br>"
+    homepage += "<br><a href=/searchQ>查詢即將上映電影</a><br>"
+
     return homepage
 
 @app.route("/mis")
@@ -183,6 +185,8 @@ def search():
     else:
         # GET 請求時，顯示搜尋表單
         return render_template("search.html")
+
+
     
 @app.route("/movie")
 def movie():
@@ -217,6 +221,25 @@ def movie():
     doc_ref = db.collection("電影").document(movie_id)
     doc_ref.set(doc)    
   return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+
+@app.route("/searchQ", methods=["POST","GET"])
+def searchQ():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""
+        db = firestore.client()     
+        collection_ref = db.collection("電影")
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
+
 
 
 
